73f30f4fad4c77e1888a2ab0b2a887be26ad5496
Lewi Samuel <arroba.lewi@gmail.com>
2023-05-24 11:08:04 -0300

    ajuste mysql

--- backend/src/lib/connect.ts
+++ backend/src/lib/connect.ts
@@ -7,15 +7,15 @@ import { Sequelize } from "sequelize";
 /**
  * Conexao com o BD via string
 */
-// const sequelize = new Sequelize(process.env.URL_CON_DB);
+const sequelize = new Sequelize(process.env.URL_CON_DB);
 
 /**
 * CONNECTION DB
 */
-var sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: 'db/database.sqlite'
-});
+// var sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: 'db/database.sqlite'
+// });
 
 export default {
     Sequelize: Sequelize,
